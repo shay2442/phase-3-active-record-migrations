3c4a00729f0612f91f6a156873aa78f12c061639
shaywacker <shay2442@yahoo.com>
2021-12-28 15:53:53 -0600

    commit tree 522a173c7e3721796b9e366628de9a98b4a532b0

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,1 +1,20 @@
 # add the Artist class here
+ActiveRecord::Base.establish_connection(
+  adapter: "sqlite3",
+  database: "db/artists.sqlite"
+)
+
+sql = <<-SQL
+  CREATE TABLE IF NOT EXISTS artists (
+  id INTEGER PRIMARY KEY,
+  name TEXT,
+  genre TEXT,
+  age INTEGER,
+  hometown TEXT
+  )
+SQL
+
+ActiveRecord::Base.connection.execute(sql)
+
+class Artist < ActiveRecord::Base
+end
